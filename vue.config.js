f4334795853b67ab10f614d806e994f42a0b2723
pravin <pray@sevadev.com>
2021-01-22 22:34:00 +0545

    updated

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dkinternational.github.io/'
     : '/',
-  devServer: {
-    disableHostCheck: true,
-  },
-  transpileDependencies: [
-    'vuetify',
-  ],
+  // devServer: {
+  //   disableHostCheck: true,
+  // },
+  // transpileDependencies: [
+  //   'vuetify',
+  // ],
 }
