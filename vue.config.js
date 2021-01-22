0d7b17f5cae48988c3d53095e154032c165cceee
pravin <pray@sevadev.com>
2021-01-22 22:49:25 +0545

    updated

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dk/'
+    ? '/dkinternational.github.io/'
     : '/',
   // devServer: {
   //   disableHostCheck: true,
